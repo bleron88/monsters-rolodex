5d118e3aa5cf98ecb0d63ad39bb0b7fbed37b818
Bleron88 <bleronselishta@gmail.com>
2020-08-28 20:01:10 +0200

    Updates

---
+++ precache-manifest.31998be215a29ded4e8f633d0a4640fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01bfb902ecf97281c5db9640ed6ff24b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "01f76bfbed8a39c57a66",
+    "url": "/monsters-rolodex/static/css/main.89ae13aa.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/monsters-rolodex/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01f76bfbed8a39c57a66",
+    "url": "/monsters-rolodex/static/js/main.7ade4862.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
